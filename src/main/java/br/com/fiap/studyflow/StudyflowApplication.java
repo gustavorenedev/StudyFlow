2003314cf9dd97d764db6863de887120ddbb87cc
Gustavo René Dias <renegustavo42@gmail.com>
2024-03-12 17:15:21 -0300

    feat: feito o setup da API

---
+++ src/main/java/br/com/fiap/studyflow/StudyflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.studyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/studyflow/StudyflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.studyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
