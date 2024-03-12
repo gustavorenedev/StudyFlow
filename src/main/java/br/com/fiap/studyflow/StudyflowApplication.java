e599778383fc67b12244538ff9896d186298e197
Gustavo René Dias <renegustavo42@gmail.com>
2024-03-12 17:44:27 -0300

    feat: Criado a main como controller

--- src/main/java/br/com/fiap/studyflow/StudyflowApplication.java
+++ src/main/java/br/com/fiap/studyflow/StudyflowApplication.java
@@ -2,12 +2,22 @@ package br.com.fiap.studyflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class StudyflowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyflowApplication.class, args);
+	}
+
+	@RequestMapping
+	@ResponseBody 
+	public String home(){
+		return "Finanças Pro";
 	}
 
 }
